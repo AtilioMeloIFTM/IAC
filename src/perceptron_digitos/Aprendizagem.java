277528f90be7e6a45fb89115fb9e06a76489f67a
AtilioMeloIFTM <atilio.faria@estudante.iftm.edu.br>
2024-08-06 19:59:29 -0300

    perceptron 4 dígitos teste

---
+++ src/perceptron_digitos/Aprendizagem.java
@@ -0,0 +1,94 @@
+package perceptron_digitos;
+
+public class Aprendizagem {
+    
+    private double x[][] = {
+        // Representações binárias para 0, 1, 2, 3
+        {1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1}, // 0
+        {1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1}, // 1
+        {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1}, // 2
+        {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},  // 3
+    };
+
+    private double w[][] = new double[16][4]; // Pesos para cada saída
+    private double t[][] = {
+        {1, -1, -1, -1},  // Saída esperada para 0
+        {-1, 1, -1, -1}, // Saída esperada para 1
+        {-1, -1, 1, -1}, // Saída esperada para 2
+        {-1, -1, -1, 1}  // Saída esperada para 3
+    }; 
+    
+    private int epocas;
+        
+    public Aprendizagem(){  
+        epocas = 0;
+    }
+    
+    public double[][] getw() {
+        return w;
+    }
+    
+    public double[][] gett() {
+        return t;
+    }    
+    
+    public int getepocas() {
+        return epocas;
+    }
+    
+    public double somatorio(int i, int k) {
+        double yent = 0;       
+        for (int j = 0; j < 16; j++)
+            yent += x[i][j] * w[j][k];
+        return yent;
+    }
+
+    public int saida(double[] yent, double limiar) {
+        int classe = 0;
+        double maiorSaida = -Double.MAX_VALUE;
+        for (int i = 0; i < yent.length; i++) {
+            if (yent[i] > maiorSaida) {
+                maiorSaida = yent[i];
+                classe = i;
+            }
+        }
+        return classe;
+    }
+    
+    public void atualiza(double alfa, double[][] f) {
+        for (int i = 0; i < 4; i++) { // Ajusta os pesos para cada classe
+            for (int j = 0; j < 16; j++) {
+                for (int k = 0; k < 4; k++) { // Atualizado para 4 saídas
+                    w[j][k] += alfa * (t[i][k] - f[i][k]) * x[i][j];
+                }
+            }
+        }
+    }
+
+    public void algoritmo(double alfa, double limiar) {
+        double[][] yent = new double[4][4]; // Para 4 classes e 4 saídas por classe
+        double[][] f = new double[4][4];
+        boolean mudou;
+
+        for (int j = 0; j < 16; j++) { // Zerando os pesos
+            for (int k = 0; k < 4; k++) {
+                w[j][k] = 0;
+            }
+        }
+
+        do {
+            mudou = false;
+            for (int i = 0; i < 4; i++) { // Para 4 classes
+                for (int k = 0; k < 4; k++) {
+                    yent[i][k] = somatorio(i, k);
+                    f[i][k] = (yent[i][k] > limiar) ? 1 : -1;
+                }
+                if (f[i][0] != t[i][0] || f[i][1] != t[i][1] || f[i][2] != t[i][2] || f[i][3] != t[i][3]) { // Verifica se houve erro
+                    mudou = true;
+                    atualiza(alfa, f);
+                }
+            }
+            epocas++;
+        } while (mudou);
+    }
+}
---
+++ src/perceptron_digitos/Validacao.java
@@ -0,0 +1,40 @@
+package perceptron_digitos;
+
+public class Validacao {
+    
+    public Validacao() {
+    }
+    
+    public double[] somatorio(int[][] mat, double[][] w) {
+        double[] yent = new double[4]; // Atualizado para 4 saídas
+        double[] entrada = new double[16];
+        int l = 1;
+        entrada[0] = 1;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                entrada[l] = mat[i][j];
+                l++;
+            }
+        }
+        for (int k = 0; k < 4; k++) { // Atualizado para 4 saídas
+            yent[k] = 0;
+            for (int j = 0; j < 16; j++) {
+                yent[k] += entrada[j] * w[j][k];
+            }
+        }
+        return yent;
+    }
+
+    public String teste(int[][] mat, double[][] w, double[][] t, double limiar) {
+        double[] yent = somatorio(mat, w);
+        int classe = 0;
+        double maiorSaida = -Double.MAX_VALUE;
+        for (int i = 0; i < yent.length; i++) {
+            if (yent[i] > maiorSaida) {
+                maiorSaida = yent[i];
+                classe = i;
+            }
+        }
+        return Integer.toString(classe);
+    }
+}
